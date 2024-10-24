280f7e20e2143740800e466c75284f4f87072aa0
serhanpehlivan <spehlivan158@gmail.com>
2024-10-24 18:03:17 +0300

    example

---
+++ schedulingtasks/src/main/java/com/example/schedulingtasks/SchedulingtasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingtasksApplication.class, args);
+	}
+
+}
