bec2036e20c28ab2b690c592332431befae109f3
serhanpehlivan <spehlivan158@gmail.com>
2024-10-24 17:40:53 +0300

    ed

--- restservice/src/main/java/com/example/restservice/RestServiceApplication.java
+++ restservice/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestserviceApplication {
+public class RestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
 
 }
