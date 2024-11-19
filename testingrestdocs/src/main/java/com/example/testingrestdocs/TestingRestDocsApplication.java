850a0dc31d1445e18fcebb648f682f620de30641
serhanpehlivan <spehlivan158@gmail.com>
2024-11-19 14:51:48 +0300

    example

---
+++ testingrestdocs/src/main/java/com/example/testingrestdocs/TestingRestDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingRestDocsApplication.class, args);
+	}
+
+}
