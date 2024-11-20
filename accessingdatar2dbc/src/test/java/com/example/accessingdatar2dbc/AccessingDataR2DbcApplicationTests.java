a5a737a071e583a540ced8191e227f03b356d87a
serhanpehlivan <spehlivan158@gmail.com>
2024-11-20 14:35:53 +0300

    example

---
+++ accessingdatar2dbc/src/test/java/com/example/accessingdatar2dbc/AccessingDataR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatar2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataR2DbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
