04e79ea9c7764d736b8284a796e7c78beb504bea
serhanpehlivan <spehlivan158@gmail.com>
2024-11-26 14:40:06 +0300

    example

---
+++ accessingdataneo4j/src/test/java/com/example/accessingdataneo4j/AccessingDataNeo4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataNeo4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
